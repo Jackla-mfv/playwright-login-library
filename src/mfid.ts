494533ed2acfe991f6aaa44ecc78af0691250a47
Kodai26 <kodai199230@gmail.com>
2025-01-03 11:25:28 +0900

    Output cookie as json file

--- src/mfid.ts
+++ src/mfid.ts
@@ -1,6 +1,7 @@
 import { expect, Page } from '@playwright/test'
 import { TOTP } from 'totp-generator';
 import { Env } from '@/src/env';
+import * as fs from 'fs';
 
 export class MFID {
   private readonly page: Page
@@ -9,7 +10,7 @@ export class MFID {
     this.page = page;
   }
 
-  public async login({email, password, redirectUrl, stateStoragePath: storageStatePath="playwright/.auth/state.json", totpSecret}: {email: string, password: string, totpSecret?: string, stateStoragePath?: string, redirectUrl?: string, }) {
+  public async login({email, password, totpSecret, redirectUrl, stateStoragePath: storageStatePath="playwright/.auth/state.json", cookieFilePath="playwright/.auth/cookie.json"}: {email: string, password: string, totpSecret?: string, stateStoragePath?: string, redirectUrl?: string, cookieFilePath?: string}) {
     // mfid login
     const emailAddressTextField = this.page.locator('//*[@id="mfid_user[email]"]');
     const loginButton = this.page.locator("#submitto");
@@ -40,6 +41,7 @@ export class MFID {
     const storageState  = await this.page.context().storageState({ path: storageStatePath });
     const cookies = await storageState.cookies;
     const formattedCookies = cookies.map((cookie) => `${cookie.name}=${cookie.value}`).join("; ");
-    return formattedCookies;
+    const formattedCookieJsonContent = JSON.stringify(formattedCookies, null, 2);
+    fs.writeFileSync(cookieFilePath, formattedCookieJsonContent);
   }
 }
