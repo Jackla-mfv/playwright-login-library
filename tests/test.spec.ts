0b7e34493ea494114132841e201ca84adf4da006
Kodai26 <kodai199230@gmail.com>
2025-01-03 11:33:20 +0900

    Add description for test

--- tests/test.spec.ts
+++ tests/test.spec.ts
@@ -2,7 +2,7 @@ import { test } from "@/src";
 import { Env } from "@/src/env";
 
 test.describe("MFID", () => {
-    test("login", async ({ mfid, page }) => {
+    test("login with valid user credential for payroll", async ({ mfid, page }) => {
         await page.goto(Env.BASE_URL);
         await mfid.login({
             email: Env.MFID_EMAIL,
